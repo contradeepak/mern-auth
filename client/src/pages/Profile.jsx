fac1b234d0133793f8e12a5575fdbc495bedb066
contradeepak <deepaktomss@gmail.com>
2024-04-09 15:19:53 +0530

    add signout user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,7 +4,7 @@ import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/
 import { app } from '../firebase';
 import { useDispatch } from 'react-redux';
 import { updateUserStart, updateUserSuccess, updateUserFailure,
-   deleteUserStart, deleteUserSuccess, deleteUserFailure } from '../redux/user/userSlice';
+   deleteUserStart, deleteUserSuccess, deleteUserFailure, signOut, } from '../redux/user/userSlice';
 
 
 
@@ -90,6 +90,17 @@ const handleDeleteAccount = async () => {
     dispatch(deleteUserFailure(error));
   }
 };
+        
+const handleSignOut = async () => {
+  try {
+    await fetch('/api/auth/signout');
+    dispatch(signOut())
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -155,8 +166,10 @@ const handleDeleteAccount = async () => {
           </button>
       </form>
       <div className='flex justify-between mt-5'>
-        <span onClick={handleDeleteAccount} className='text-red-700 cursor-pointer'>Delete Account</span>
-        <span className='text-red-700 cursor-pointer'>Sign out</span>
+        <span onClick={handleDeleteAccount} 
+        className='text-red-700 cursor-pointer'>Delete Account</span>
+
+        <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
       <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
       <p className='text-green-700 mt-5'>{updateSuccess && 'User is updated successfully!'}</p>
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -49,6 +49,11 @@ const userSlice = createSlice({
             state.loading = false;
             state.error = action.payload;
         },
+        signOut: (state) => {
+        state.currentUser = null;
+        state.loading = false;
+        state.error = false;
+    },
     },
 });
 
@@ -59,9 +64,10 @@ export const {
      updateUserFailure, 
      updateUserStart,
      updateUserSuccess,
-      deleteUserFailure,
-      deleteUserStart,
-      deleteUserSuccess,
+     deleteUserFailure,
+     deleteUserStart,
+     deleteUserSuccess,
+     signOut,
 } = userSlice.actions;
 
 export default userSlice.reducer;
