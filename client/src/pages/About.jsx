796406597cef5d9426fbc1c02f61f42c9bda9c3a
contradeepak <deepaktomss@gmail.com>
2024-04-09 15:46:10 +0530

    complete home and about pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,24 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+    <h1 className='text-3xl font-bold mb-4
+    text-slate-800'>About</h1>
+    <p className='mb-4 text-slate-700'>This is a full-stack web application built with the 
+      MERN (MongoDB, Express, React, Node.js) stack. It 
+      includes authentication features that allow users to
+      signup, log in, and log out, and provides access to 
+      protected routes only for authenticated users.
+    </p>
+    <p className='mb-4 text-slate-700'>The front-end of the applicationis built with React and uses React 
+    Router for client-side routing. The backend is  built 
+    with Nodejs and Express, and uses MongoDB as the 
+    database. Authentication is implemented using JSON Web Tokens(JWT).
+    </p>
+    <p className='mb-4 text-slate-700'>This application is intended as 
+    a starting point for building full-stack web applications using the 
+    MERN stack. Feel free to use it as a template for your own projects!.
+    </p>
+  </div>
   )
 }
